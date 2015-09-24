feda3e3de741e0e7d91b719040652d87db60385a
steven harradine <steven.harradine@telus.com>
2015-09-24 07:58:03 -0400

    added parser

---
+++ parsers/xbox-live.js
@@ -0,0 +1,14 @@
+module.exports.name = "xbox Live"
+module.exports.url = "http://support.xbox.com/en-CA/xbox-live-status"
+
+module.exports.parse = function (dom) {
+  var isError = true
+
+  dom(".service").each (function (i, el) {
+    if (dom(this).attr("class", "active").length) {
+      isError = false
+    }
+  })
+
+  return isError
+}
