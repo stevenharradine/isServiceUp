1092d5e0bafbadb90abc707b591a915bab54b200
steven harradine <steven.harradine@telus.com>
2015-09-24 15:27:54 -0400

    add extensibleish alerts (need to figure out the package.json issue)

---
+++ alerts/email.js
@@ -0,0 +1,37 @@
+var CONFIG       = require("./../config"),
+    nodemailer   = require("nodemailer")
+
+module.exports.alert = function (isError, callback) {
+  var sentMailCounter = 0,
+      subject         = parser.name + " is " + (isError ? "down" : "up"),
+      transporter     = nodemailer.createTransport({
+    service: CONFIG.EMAIL_PROVIDER,
+    auth: {
+      user: CONFIG.EMAIL_USER,
+      pass: CONFIG.EMAIL_PASSWORD
+    }
+  })
+
+  CONFIG.ALERT_LIST.forEach (function (emailAddress, index, array) {
+    var mailOptions = {               // setup e-mail data with unicode symbols
+      from: "Service Watch ✔ <Service-Watch@alert.com>", // sender address
+      to: emailAddress,               // list of receivers
+      subject: subject,               // Subject line
+      text: subject,                  // plaintext body
+      html: subject                   // html body
+    }
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error){
+        console.log(error)
+      } else {
+        console.log("Message sent: " + info.response)
+      }
+
+      if (++sentMailCounter == CONFIG.ALERT_LIST.length) {
+        callback ()
+      }
+    })
+  })
+}
--- isServiceUp.js
+++ isServiceUp.js
@@ -2,7 +2,6 @@ var CONFIG       = require("./config"),
     fs           = require('fs')
     request      = require('request'),
     Cheerio      = require('cheerio'),
-    nodemailer   = require("nodemailer"),
     parser       = require ("./parsers/" + process.argv[2] + ".js")
 
 request(parser.url, function (error, response, body) {
@@ -38,11 +37,9 @@ function readStatus (statusFile, isError, callback) {
 }
 
 function alert (isError) {
-  if (CONFIG.ALERT_METHOD == "email") {
-    sendEmails (parser.name + " is " + (isError ? "down" : "up"), function () {
-      console.log ("done")
-    })
-  }
+  require ('./alerts/' + CONFIG.ALERT_METHOD + ".js").alert(isError, function () {
+    console.log ("done")
+  })
 }
 
 function writeStatus (statusFile, isError, callback) {
@@ -54,39 +51,5 @@ function writeStatus (statusFile, isError, callback) {
     console.log ("Status file updated")
 
     callback ()
-  })
-}
-
-function sendEmails (subject, callback) {
-  var sentMailCounter = 0,
-      transporter     = nodemailer.createTransport({
-    service: CONFIG.EMAIL_PROVIDER,
-    auth: {
-      user: CONFIG.EMAIL_USER,
-      pass: CONFIG.EMAIL_PASSWORD
-    }
-  })
-
-  CONFIG.ALERT_LIST.forEach (function (emailAddress, index, array) {
-    var mailOptions = {               // setup e-mail data with unicode symbols
-      from: "Service Watch ✔ <Service-Watch@alert.com>", // sender address
-      to: emailAddress,               // list of receivers
-      subject: subject,               // Subject line
-      text: subject,                  // plaintext body
-      html: subject                   // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error){
-        console.log(error)
-      } else {
-        console.log("Message sent: " + info.response)
-      }
-
-      if (++sentMailCounter == CONFIG.ALERT_LIST.length) {
-        callback ()
-      }
-    })
   })
 }
