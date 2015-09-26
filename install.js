d79a06953c382e44fc5212bbc8224ddf57c634ec
steven harradine <steven.harradine@telus.com>
2015-09-25 22:38:25 -0400

    clean up installer; package.json is installed the same way as the alert package files

--- install.js
+++ install.js
@@ -3,26 +3,24 @@ var fs           = require('fs'),
     exec         = require('child_process').exec,
     getFilesSync = require('./libs/getFilesSync').getFilesSync
 
-exec ("npm install", function (error, stdout, stderr) {
-  if (error) {
-    buffered_out += error
-  }
-
-  console.log (stdout)
-  getFilesSync ("alerts", [], "package.json").forEach (function (alert, index, array) {
-    var data = require ("./" + alert),
-        keys = Object.keys(data.dependencies)
-
-      keys.forEach (function (dependency, index, array) {
-        var cmd = "npm install " + dependency
-        console.log (cmd)
-        exec (cmd, function (error, stdout, stderr) {
-          if (error) {
-            buffered_out += error
-          }
-
-          console.log (stdout)
-        })
-      })
-  })
+installDependancies ("package.json")
+getFilesSync ("alerts", [], "package.json").forEach (function (package_path, index, array) {
+  installDependancies (package_path)
 })
+
+function installDependancies (package_path) {
+  var data = require ("./" + package_path),
+    keys = Object.keys(data.dependencies)
+
+  keys.forEach (function (dependency, index, array) {
+    var cmd = "npm install " + dependency
+    console.log (cmd)
+    exec (cmd, function (error, stdout, stderr) {
+      if (error) {
+        buffered_out += error
+      }
+
+      console.log (stdout)
+    })
+  })
+}
