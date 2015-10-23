2459aab6d2cd5797a9d830903e65e3fd844aea65
Steven Harradine <stevenharradine@users.noreply.github.com>
2015-10-22 20:34:50 -0400

    flip the isError true/false so it works

--- parsers-available/bitbucket.js
+++ parsers-available/bitbucket.js
@@ -2,10 +2,10 @@ module.exports.name = "BitBucket"
 module.exports.url = "http://status.bitbucket.org/"
 
 module.exports.parse = function (dom) {
-  var isError = false
+  var isError = true
 
   if (dom(".status-index").find(".container").find(".page-status").find(".status").text().trim() == "All systems operational") {
-    isError = true
+    isError = false
   }
 
   return isError
