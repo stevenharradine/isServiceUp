46801ac44a5a20716771df20cb7c3b752b33a715
steven harradine <steven.harradine@telus.com>
2015-10-25 15:35:59 -0400

    remove unneed var

--- parsers-available/skype.js
+++ parsers-available/skype.js
@@ -2,8 +2,6 @@ module.exports.name = "Skype"
 module.exports.url = "http://heartbeat.skype.com/"
 
 module.exports.parse = function (dom) {
-  var isError = false
-
   dom("#content").find(".post").each (function (i, el) {
     if (dom(this).find (".post-status").find(".issue-ongoing").length) {
       return "Some issues"
