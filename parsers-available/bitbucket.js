545c4f8c41326af2cc9e0a0035a14ee24dfdc7f1
steven harradine <steven.harradine@telus.com>
2015-10-25 15:35:43 -0400

    clean up

--- parsers-available/bitbucket.js
+++ parsers-available/bitbucket.js
@@ -2,11 +2,5 @@ module.exports.name = "BitBucket"
 module.exports.url = "http://status.bitbucket.org/"
 
 module.exports.parse = function (dom) {
-  var isError = true
-return dom(".status-index").find(".container").find(".page-status").find(".status").text().trim()
-  if (dom(".status-index").find(".container").find(".page-status").find(".status").text().trim() == "All systems operational") {
-    isError = false
-  }
-
-  return isError
+  return dom(".status-index").find(".container").find(".page-status").find(".status").text().trim()
 }
